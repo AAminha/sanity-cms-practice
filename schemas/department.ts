f9699c491d6daea25af029b931cc4b222ab8a7fb
aaminha <dks4857@gmail.com>
2024-01-25 15:35:22 +0900

    feat: 한마디 스키마 설계

--- schemas/department.ts
+++ schemas/department.ts
@@ -39,5 +39,23 @@ export default defineType({
       type: 'array',
       of: [{type: 'videoInformation'}],
     }),
+    defineField({
+      name: 'inaWord',
+      title: '한 마디',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'title',
+          title: '제목',
+          type: 'string',
+        }),
+        defineField({
+          name: 'content',
+          title: '내용',
+          type: 'text',
+          description: '소개 페이지에서 줄바꿈도 반영됩니다. 이를 고려해서 작성해주세요.',
+        }),
+      ],
+    }),
   ],
 })
